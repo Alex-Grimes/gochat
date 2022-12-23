aa7e8ffc69009f086ee41ee08716b95caba55392
Alex <AlexGrimes.se@gmail.com>
2022-12-22 20:44:55 -0500

    working on web socket connections

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,53 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-import "fmt"
+	"github.com/gorilla/websocket"
+)
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 
-func main() {
-    fmt.Println("Chat app v0.01")
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWsw(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println(r.Host)
+
+	ws, err := nil
+
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Simple Server")
+	})
+}
+
+func main() {
+
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
